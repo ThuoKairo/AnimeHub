da740e04b669d18eb9a94d86569fe3013fe3a8be
ThuoKairo <kairo.thuo@strathmore.edu>
2025-11-03 09:24:04 +0300

    Add review form and submission functionality to anime detail page

--- anime/admin.py
+++ anime/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Anime, UserAnimeList, Review
 
-# Register your models here.
+admin.site.register(Anime)
+admin.site.register(UserAnimeList)
+admin.site.register(Review)
--- anime/views.py
+++ anime/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Anime, Review
+from .forms import ReviewForm
 
-# Create your views here.
+def anime_detail(request, anime_id):
+    anime = get_object_or_404(Anime, pk=anime_id)
+    reviews = Review.objects.filter(anime=anime)
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        if form.is_valid():
+            review = form.save(commit=False)
+            review.anime = anime
+            review.user = request.user
+            review.save()
+            return redirect('anime_detail', anime_id=anime.id)
+    else:
+        form = ReviewForm()
+    return render(request, 'anime/anime_detail.html', {
+        'anime': anime,
+        'reviews': reviews,
+        'form': form
+    })
