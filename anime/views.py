7af023d5899da14354a9fd7e1b2e02926e89e946
ThuoKairo <kairo.thuo@strathmore.edu>
2025-11-19 09:14:37 +0300

    Styling

--- anime/views.py
+++ anime/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Anime, Review
 from .forms import ReviewForm
 
+def anime_list(request):
+    animes = Anime.objects.all()
+    return render(request, 'anime/anime_list.html', {'animes': animes})
+
 def anime_detail(request, anime_id):
     anime = get_object_or_404(Anime, pk=anime_id)
     reviews = Review.objects.filter(anime=anime)
